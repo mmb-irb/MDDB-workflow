64fd4aafff91db11e4edaefa6107a7e705490326
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2021-02-19 11:40:38 +0100

    new backbone system more compatible with pca

--- model_workflow/tools/get_backbone.py
+++ model_workflow/tools/get_backbone.py
@@ -11,5 +11,10 @@ def get_backbone (
     output_backbone_filename : str):
 
     topology = topology_reference.topology
-    backbone = topology.select('backbone')
+    # WARNING: The default 'backbone' prody selection is equal to 'protein and name N CA C O'
+    # WARNING: The equivalent 'Backbone' gromacs selection would be 'name N CA C' in prody
+    # Since this backbone is used with the PCA results, which come from gromacs, it must match
+    # Oxygens are removed, since gromacs results do not include oxygens (e.g. all backbone oxygens)
+    # Protein restriction is removed, since gromacs would consider other things (e.g. caping residues)
+    backbone = topology.select('name N CA C')
     prody.writePDB(output_backbone_filename, backbone)
