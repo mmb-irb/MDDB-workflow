b751e1a9f312405fc0d86d2a5713550651a01ba0
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2021-05-14 11:41:28 +0200

    production setup includes resources files

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
+from glob import glob
+
+# Include all files in the resources directory
+# Exclude the 'deprecated' directory
+resources_directory = 'model_workflow/utils/resources'
+deprecated_directory = resources_directory + '/deprecated'
+resources_files = [ log for log in glob(resources_directory + '/*') if log != deprecated_directory ]
+
+print(resources_files)
+
 setup(
     name='model_workflow',
     version='0.0.1',
@@ -11,4 +21,10 @@ setup(
         'console_scripts': [
             'mwf = model_workflow.mwf:main',
         ]
-    },)
+    },
+    data_files=[
+        # Include all files in the resources directory but not recursively
+        # This way, files in the 'dprecated' directory are ignored
+        (resources_directory, resources_files)
+    ]
+)
