d2b8c657e9866f6686b6c65dae68eeaaeae7d5d1
dbeltran <daniel.beltran@irbbarcelona.org>
2023-03-15 14:39:33 +0100

    rgyr analysis is now exported to json

--- model_workflow/analyses/rgyr.py
+++ model_workflow/analyses/rgyr.py
@@ -1,6 +1,13 @@
-import os
+from os.path import exists
+from os import remove
 from subprocess import run, PIPE, Popen
+from numpy import mean, std
+from json import dump
 from model_workflow.tools.get_reduced_trajectory import get_reduced_trajectory
+from model_workflow.tools.xvg_parse import xvg_parse
+
+# Set an auxiliar data filename
+rgyr_data_filename = '.rgyr_data.xvg'
 
 # Radius of gyration (Rgyr)
 # 
@@ -10,7 +17,7 @@ def rgyr (
     input_topology_filename : str,
     input_trajectory_filename : str,
     output_analysis_filename : str,
-    frames_limit : int) -> str:
+    frames_limit : int):
 
     # Use a reduced trajectory in case the original trajectory has many frames
     reduced_trajectory_filename, step, frames = get_reduced_trajectory(
@@ -32,15 +39,58 @@ def rgyr (
         "-f",
         reduced_trajectory_filename,
         '-o',
-        output_analysis_filename,
+        rgyr_data_filename,
         '-quiet'
     ], stdin=p.stdout, stdout=PIPE, stderr=PIPE).stdout.decode()
     p.stdout.close()
 
     # If the output does not exist at this point it means something went wrong with gromacs
-    if not os.path.exists(output_analysis_filename):
+    if not exists(rgyr_data_filename):
         print(logs)
         raise SystemExit('Something went wrong with GROMACS')
 
-    # Return gromacs logs
-    return logs
+    # Read the output file and parse it
+    raw_rgyr_data = xvg_parse(rgyr_data_filename, ['times', 'rgyr', 'rgyrx', 'rgyry', 'rgyrz'])
+
+    # Cleanup the auxiliar file
+    remove(rgyr_data_filename)
+
+    # Format data
+    rgyr_data = {
+        'start': 0,
+        'step': step,
+        'y': {
+            'rgyr': {
+                'average': mean(raw_rgyr_data['rgyr']),
+                'stddev': std(raw_rgyr_data['rgyr']),
+                'min': min(raw_rgyr_data['rgyr']),
+                'max': max(raw_rgyr_data['rgyr']),
+                'data': raw_rgyr_data['rgyr']
+            },
+            'rgyrx': {
+                'average': mean(raw_rgyr_data['rgyrx']),
+                'stddev': std(raw_rgyr_data['rgyrx']),
+                'min': min(raw_rgyr_data['rgyrx']),
+                'max': max(raw_rgyr_data['rgyrx']),
+                'data': raw_rgyr_data['rgyrx']
+            },
+            'rgyry': {
+                'average': mean(raw_rgyr_data['rgyry']),
+                'stddev': std(raw_rgyr_data['rgyry']),
+                'min': min(raw_rgyr_data['rgyry']),
+                'max': max(raw_rgyr_data['rgyry']),
+                'data': raw_rgyr_data['rgyry']
+            },
+            'rgyrz': {
+                'average': mean(raw_rgyr_data['rgyrz']),
+                'stddev': std(raw_rgyr_data['rgyrz']),
+                'min': min(raw_rgyr_data['rgyrz']),
+                'max': max(raw_rgyr_data['rgyrz']),
+                'data': raw_rgyr_data['rgyrz']
+            }
+        }
+    }
+
+    # Export formatted data to a json file
+    with open(output_analysis_filename, 'w') as file:
+        dump(rgyr_data, file)
