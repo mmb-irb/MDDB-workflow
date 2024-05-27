b43f680497f234a236f410da4a84301362ff014c
dbeltran <daniel.beltran@irbbarcelona.org>
2024-05-27 12:28:05 +0200

    changed intrajrity log system for sbatch logs fix

--- model_workflow/analyses/rmsd_check.py
+++ model_workflow/analyses/rmsd_check.py
@@ -64,9 +64,10 @@ def check_trajectory_integrity (
     # Save all RMSD jumps
     rmsd_jumps = []    
 
+    # Iterate trajectory frames
     for f, frame in enumerate(trajectory, 1):
         # Update the current frame log
-        print(' Frame ' + str(f), end='\r')
+        print(f'\r Frame {f}', end='')
 
         # Calculate RMSD value between previous and current frame
         rmsd_value = mdt.rmsd(frame, previous_frame, atom_indices=parsed_selection.atom_indices)[0]
@@ -74,6 +75,9 @@ def check_trajectory_integrity (
 
         # Update the previous frame as the current one
         previous_frame = frame
+
+    # Add an extra breakline after the last log
+    print()
 
     # If the trajectory has only 1 or 2 frames then there is no test to do
     if len(rmsd_jumps) <= 1:
@@ -111,11 +115,11 @@ def check_trajectory_integrity (
             if outliers_count >= 4:
                 print(' etc...')
                 break
-            print(' FAIL: Sudden RMSD jump between frames ' + str(i) + ' and ' + str(i+1))
+            print(f' FAIL: Sudden RMSD jump between frames {i} and {i+1}')
             outliers_count += 1
 
     # Always print the maximum z score and its frames
-    print(' Maximum z score (' + str(max_z_score) + ') reported between frames ' + str(max_z_score_frame) + ' and ' + str(max_z_score_frame + 1))
+    print(f' Maximum z score {max_z_score} reported between frames {max_z_score_frame} and {max_z_score_frame + 1}')
 
     # If there were any outlier then the check has failed
     if outliers_count > 0:
