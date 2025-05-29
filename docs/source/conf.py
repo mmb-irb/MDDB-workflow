00914374b8632dfea7a87f0a8ccf69bebdbf5134
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-05-29 12:46:33 +0200

    add link to source code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import inspect
+
 # Add the path to your project's root directory
 sys.path.insert(0, os.path.abspath('../../')) # Adjust this path if your 'my_package' is elsewhere
 
@@ -14,6 +16,8 @@ sys.path.insert(0, os.path.abspath('../../')) # Adjust this path if your 'my_pac
 project = 'MDDB_Workflow'
 copyright = '2025, IRB Barcelona'
 author = 'IRB Barcelona'
+github_user = 'mmb-irb'
+github_repo = 'MDDB-workflow'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,6 +30,7 @@ extensions = [
     'sphinx.ext.napoleon',      # To parse Google/NumPy style docstrings
     'sphinx_rtd_theme',         # ReadTheDocs theme
     'myst_parser',              # For Markdown support
+    'sphinx.ext.linkcode',      # For linking to source code
 ]
 
 # Napoleon settings
@@ -46,7 +51,35 @@ html_theme_options = {'style_nav_header_background': "#FFFFFF",}
 html_static_path = ['_static']
 html_context = {
     'display_github': True,
-    'github_user': 'mmb-irb',
-    'github_repo': 'MDDB-workflow',
+    'github_user': github_user,
+    'github_repo': github_repo,
     'github_version': 'master/docs/source/',
 }
+
+
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+
+    # Get the module and object
+    module = sys.modules.get(info['module'])
+    if module is None:
+        return None
+
+    obj = module
+    for part in info['fullname'].split('.'):
+        try:
+            obj = getattr(obj, part)
+        except AttributeError:
+            return None
+
+    # Try to get the file path and line number
+    try:
+        filepath = os.path.relpath(inspect.getsourcefile(obj), start=os.path.abspath('../')) # Adjust '..' to your repo root
+        lineno = inspect.getsourcelines(obj)[1]
+    except (TypeError, OSError):
+        return None
+
+    return f"https://github.com/{github_user}/{github_repo}/blob/master/{filepath}#L{lineno}"
