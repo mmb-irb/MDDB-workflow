efc01d28a476eafac4fd5771944ade6a61b01271
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-07-21 15:19:13 +0200

    Add nassa test

--- test/test_nassa.py
+++ test/test_nassa.py
@@ -1,8 +1,8 @@
 import os
 import pytest
+import subprocess
 from model_workflow.utils.constants import *
 from model_workflow.utils.type_hints import *
-
 
 @pytest.mark.release
 class TestNassa:
@@ -14,5 +14,24 @@ class TestNassa:
     def test_helical(self, project: 'Project'):
         project.mds[0].run_helical_analysis(project)
     
-    # Test Workflow
-    # mwf nassa -hp -pdirs . -stru replica_1/structure.pdb -traj replica_1/trajectory.xtc -top topology.prmtop -all
+    
+    def test_nassa_helical(self, test_data_dir: str):
+        os.chdir(os.path.join(test_data_dir, 'output'))
+        command = "mwf nassa -hp -stru *.prmtop -traj *.xtc -top *.prmtop  -pdirs seq001-1 -mdir replica_1 -m"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        assert result.returncode == 0
+        # Add more assertions based on the expected output or side effects
+
+    def test_nassa_create_config(self, test_data_dir: str):
+        os.chdir(os.path.join(test_data_dir, 'output'))
+        command = f"mwf nassa -w seq001-1/replica_1 -seq {test_data_dir}/input"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        assert result.returncode == 0
+        # Add more assertions based on the expected output or side effects
+
+    def test_nassa_all(self, test_data_dir: str):
+        os.chdir(os.path.join(test_data_dir, 'output'))
+        command = "mwf nassa -c nassa.yml -all"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        assert result.returncode == 0
+        # Add more assertions based on the expected output or side effects
