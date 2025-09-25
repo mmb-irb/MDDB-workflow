932107f8c2343b1df2e51bad7f25de698d2a50ea
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-09-25 15:42:25 +0200

    Fix linter for glucolipids

--- model_workflow/analyses/lipid_interactions.py
+++ model_workflow/analyses/lipid_interactions.py
@@ -22,12 +22,10 @@ def lipid_interactions (
     output_analysis_filepath = f'{output_directory}/{OUTPUT_LIPID_INTERACTIONS_FILENAME}'
     
     frame_step, frame_count = calculate_frame_step(snapshots, frames_limit)
-    lipids = sorted(list(set([ lipid['name'] for lipid in lipid_map])))
-    lipids_str = " ".join(lipids)
-    # Get the lipid residue indices
-    lipids_residx = universe.select_atoms('resname ' + lipids_str).residues.resindices
+    lipids_residx = [residue_idx for lipid in lipid_map for residue_idx in lipid['residue_indices']]
     # Create a mapping from lipid residue indices to array indices
-    lipid_to_idx = {lipid: i for i, lipid in enumerate(lipids_residx)}
+    residx_2_idx = {residx: i for i, residx in enumerate(lipids_residx)}
+    lipid_group = universe.select_atoms(f'resindex {" ".join(map(str, lipids_residx))}')
     # A counter for each pair of protein-lipid residues
     protein_residx = universe.select_atoms('protein').residues.resindices
     ocupancy_arrs = np.zeros((len(protein_residx), len(lipids_residx)))
@@ -35,7 +33,8 @@ def lipid_interactions (
     # Only iterate through the frames you need. TODO: parallel frames
     for ts in universe.trajectory[0:snapshots:frame_step]:
         # Select lipid atoms near the protein once
-        lipid_near_prot = universe.select_atoms(f'(around 6 protein) and (resname {lipids_str}) and not protein')
+        lipid_near_prot = universe.select_atoms(f'(around 6 protein) and group lipid_group and not protein', 
+                                                lipid_group=lipid_group)
         for i, residx in enumerate(protein_residx):
             # Find lipid atoms near a specific residue
             residue_atoms = universe.select_atoms(f'resindex {residx}')
@@ -43,7 +42,7 @@ def lipid_interactions (
                                                 residuegroup=residue_atoms)
             # Add the count of lipids
             for lipid_residx in lipid_near_res.residues.resindices:
-                ocupancy_arrs[i, lipid_to_idx[lipid_residx]] += 1
+                ocupancy_arrs[i, residx_2_idx[lipid_residx]] += 1
 
     # Normalize the occupancy arrays by dividing by the number of frames
     ocupancy_arrs /= frame_count
@@ -52,7 +51,7 @@ def lipid_interactions (
     data = { 'residue_indices': protein_residx.tolist()}
     for lipid in lipid_map:
         # Convert lipid residue indices to array indices
-        lipid_idx = [lipid_to_idx[lipid_residx] for lipid_residx in lipid['residue_indices']]
+        lipid_idx = [residx_2_idx[residx] for residx in lipid['residue_indices']]
         data[lipid['match']['ref']['inchikey']] = ocupancy_arrs[:, lipid_idx].sum(1).tolist()
     # Wrap the data in a dictionary
     data = { 'data': data}
