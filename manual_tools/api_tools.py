3f7f219dff488648775797fc70dca5650258ab17
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-10-21 13:01:38 +0200

    Fix query brackets and returns projects

--- manual_tools/api_tools.py
+++ manual_tools/api_tools.py
@@ -5,10 +5,12 @@ from model_workflow.console import DEFAULT_API_URL
 # The URL to the API from our node
 base_url = DEFAULT_API_URL+'rest/current'
 
-def query_api (url : str) -> dict:
+def query_api (url : str, verbose=False) -> dict:
     """ Parse the URL in case it contains any HTTP control characters
     Replace white spaces by the corresponding percent notation character. """
     parsed_url = url.replace(" ", "%20")
+    if verbose:
+        print(f'Querying API URL: {parsed_url}')
     with urllib.request.urlopen(parsed_url) as response:
         return json.loads(response.read().decode("utf-8"))
     
@@ -21,12 +23,13 @@ def count_projects(query_url):
 
 def paginate_projects(selection_query, limit = 100):
     # Ask which projects have a tpr file
-    query_url = base_url + f'/projects?query={selection_query}'
+    query_url = base_url + f'/projects?query={{{selection_query}}}'
     n_projects = count_projects(query_url)
     # Calculate the expected number of pages
     pages = ceil(n_projects / limit)
     # Set a list to store all the mined accession values
     accessions = []
+    projects_list = []
     # Iterate over pages
     # Note that pages are 1-base numerated, and NOT 0-based
     for page in range(1, pages + 1):
@@ -38,11 +41,12 @@ def paginate_projects(selection_query, limit = 100):
         response = query_api(paginated_url)
         # Mine target data
         projects = response['projects']
+        projects_list.extend(projects)
         project_accessions = [ project['accession'] for project in projects ]
         accessions += project_accessions
         
     print(f'We have mined {len(accessions)} project accessions')
-    return accessions
+    return accessions, projects_list
 
 def download_files(accessions: list, file: str, output_folder: str):
     # Store all JSON topologies in a list
