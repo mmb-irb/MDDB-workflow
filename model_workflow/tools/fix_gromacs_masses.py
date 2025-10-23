2a415b661a93b17c82e895f93437079307fbe2dd
dbeltran <daniel.beltran@irbbarcelona.org>
2025-10-23 16:17:11 +0200

    fix in gromacs masses fixer

--- model_workflow/tools/fix_gromacs_masses.py
+++ model_workflow/tools/fix_gromacs_masses.py
@@ -42,4 +42,8 @@ def fix_gromacs_masses ():
 
     # Check if the backup file exists and, if not, then rename the current masses file as the backup
     if not local_custom_masses_file.exists:
+        # If it does not exists then it means it is a symlink pointing to a not valid direction
+        # This may happend when working in different machines
+        # Simply remove the old symlink
+        if local_custom_masses_file.is_symlink: local_custom_masses_file.remove()
         local_custom_masses_file.set_symlink_to(source_custom_masses_file)
