ea9e3cdd27f584b6797405fbbd4d0d0c356d4366
RubenChM <ruben.chaves@irbbarcelona.org>
2026-02-12 09:20:19 +0100

    Change mwf_run_cmd CLI arg type

--- mddb_workflow/console.py
+++ mddb_workflow/console.py
@@ -821,7 +821,7 @@ ds_scan = dataset_subparsers.add_parser("scan", formatter_class=CustomHelpFormat
 # # Dataset run subcommand
 ds_run = dataset_subparsers.add_parser("run", formatter_class=CustomHelpFormatter, help="Run the workflow for a dataset of MDDB projects.", parents=[common_ds_parser, query_parser])
 ds_run.add_argument("-n", "--n_jobs", type=int, default=0, help=ds_help['launch_workflow']['n_jobs'])
-ds_run.add_argument("-c", "--mwf_run_cmd", type=int, default=0, help=ds_help['launch_workflow']['mwf_run_cmd'])
+ds_run.add_argument("-c", "--mwf_run_cmd", type=str, help=ds_help['launch_workflow']['mwf_run_cmd'])
 ds_run.add_argument("-sl", "--slurm", action="store_true", help=ds_help['launch_workflow']['slurm'])
 ds_run.add_argument("-jt", "--job-template", help=ds_help['launch_workflow']['job_template'])
 ds_run.add_argument("--debug", action="store_true", help=ds_help['launch_workflow']['debug'])
